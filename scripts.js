0fb98327b7af9e653218f66863ab341e4f7a71fd
ColeCz <124841853+ColeCz@users.noreply.github.com>
2024-01-30 12:51:02 -0500

    added scripts.js

---
+++ scripts.js
@@ -0,0 +1,24 @@
+function send_email() {
+    var user_email = document.getElementById('user_email').value;
+    var user_message = document.getElementById('user_message').value;
+}
+
+function open_instagram() {
+    window.location.href = 'https://www.instagram.com/center_aisle_events?igsh=M3oxY3NtZTVvMnFo';
+}
+
+function to_about() {
+    window.location.href = 'about.html';
+}
+
+function to_florals() {
+    window.location.href = 'florals.html';
+}
+
+function to_planning() {
+    window.location.href = 'planning.html';
+}
+
+function to_decor() {
+    window.location.href = 'decor.html';
+}
